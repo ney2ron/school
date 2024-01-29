99f3d499b3ff72e886569196f4d7cf2900e128fb
ney2ron <13kit16@bk.ru>
2024-01-29 11:59:13 +0300

    Стеки

---
+++ steck.py
@@ -0,0 +1,21 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+    
+    def push(self.item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        last = len(self.items)-1
+        return self.tems [last]
+    
+    def size(self):
+        return len(self.items)
+    
+
